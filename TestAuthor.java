08501b3d0e01c13b1fd054d6de7280efd413a8fd
Jolengo <70565165+Jolengo@users.noreply.github.com>
2020-09-14 21:37:18 +0300

    Add files via upload

---
+++ TestAuthor.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author q = new Author("Dima","Dima@mail.ru",'M');
+        Author w = new Author("Rare","rare-@mail.ru",'F');
+        System.out.println(q);
+        System.out.println(w);
+
+    }
+}
